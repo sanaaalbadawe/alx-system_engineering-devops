987ae138d2092117d930381c45ba9a7c9cc8d8f2
sanaaalbadawe <sanaaalbadawe9@gmail.com>
2023-08-13 07:09:51 -0700

    task 1213141516171819202122

--- 0x02-shell_redirections/12-newest_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the 10 newest files in the current directory
-newest_files=$(ls -lt | grep '^-' | head -n 10)
-
-# Display the newest files
-echo "The 10 newest files in the current directory are:"
-echo "$newest_files"
