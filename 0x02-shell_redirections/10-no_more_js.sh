2138fa24adc2ce785a3cfb2625ce795301ad6f84
sanaaalbadawe <sanaaalbadawe9@gmail.com>
2023-08-13 06:42:29 -0700

    task 345678910

--- 0x02-shell_redirections/10-no_more_js.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Recursively find all regular files with .js extension and delete them
-find . -type f -name "*.js" -delete
-
-# Provide confirmation message
-echo "All regular files with .js extension have been deleted."
--- 0x02-shell_redirections/8-cwd_state.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Execute ls -la command and redirect the output to ls_cwd_content
-ls -la > ls_cwd_content
-
-# Provide confirmation message
-echo "The result of ls -la command has been written to ls_cwd_content"
